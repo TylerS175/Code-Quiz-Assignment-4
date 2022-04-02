4aa92f49fa44d9a81640b8a7bf0280e5a1d6ac89
Your name <tylershifflett@Tylers-MacBook-Pro.local>
2022-04-02 15:37:09 -0400

    Add Timer and Questions

---
+++ assets/javascript.js
@@ -0,0 +1,40 @@
+// Create 5 questions using the Var // 
+
+var questions = {
+    {}
+    title: "/";
+    choices: "/";
+    answer: "/";
+    };
+    {
+    title: "/";
+    choices: "/";
+    answer: "/";
+    };
+    {
+    title: "/";
+    choices: "/";
+    answer: "/"
+    };
+    {
+    title: "/";
+    choices: "/";
+    answer: "/";
+    };
+   {
+    title: "/";
+    choices: "/";
+    answer: "/";
+});
+
+// Variables on Score & Question Index //
+var score = 0;
+var questionIndex = 0;
+
+//Creating a Timer with Var//
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+// Adding Penalty and Countdown 
